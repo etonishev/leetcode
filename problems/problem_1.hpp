642b617e7abbb151c3a8831dec81eba7379873fc
Eugene G. Tonishev <tonisheg@yandex.ru>
2022-10-03 00:38:41 +0600

    Refactor the first problem solution

--- problems/problem_1.hpp
+++ problems/problem_1.hpp
@@ -4,20 +4,17 @@
 class Solution {
  public:
   std::vector<int> twoSum(std::vector<int>& nums, int target) {
-    std::map<int, std::size_t> map;
-    std::vector<int> result;
+    std::map<int, std::size_t> hashTable;
+
     for (std::size_t i = 0; i < nums.size(); ++i) {
-      int& currentValue = nums[i];
-      auto it = map.find(target - currentValue);
-      if (it == map.end()) {
-        map.insert(std::make_pair(currentValue, i));
-      } else {
-        result.push_back(it->second);
-        result.push_back(i);
-        return result;
+      auto it = hashTable.find(target - nums[i]);
+      if (it != hashTable.end()) {
+        return {static_cast<int>(it->second), static_cast<int>(i)};
       }
+
+      hashTable.insert(std::make_pair(nums[i], i));
     }
 
-    return result;
+    return {};
   }
 };
